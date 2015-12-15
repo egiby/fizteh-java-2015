a73aa38be569b97ca0921d8fcfd43cb3196c20f1
egiby <egiby@egiby>
2015-12-16 00:05:14 +0300

    moduletests init

---
+++ egiby/src/main/java/ru/fizteh/fivt/students/egiby/moduletests/TwitterStream.java
@@ -0,0 +1,36 @@
+package ru.fizteh.fivt.students.egiby.moduletests;
+
+import com.beust.jcommander.JCommander;
+import twitter4j.TwitterException;
+
+import ru.fizteh.fivt.students.egiby.twitterstream.library.*;
+
+/**
+ * Created by egiby on 24.09.15.
+ */
+
+public class TwitterStream {
+    public static void main(String[] args) {
+        JCommanderParams jcp = new JCommanderParams();
+        JCommander jcm = new JCommander(jcp, args);
+
+        if (jcp.isHelp()) {
+            TwitterPrinter.printHelp(jcm);
+            System.exit(0);
+        }
+
+        if (jcp.isStream()) {
+            if (jcp.getNumberTweets() != null) {
+                TwitterPrinter.printHelp(jcm);
+                System.exit(0);
+            }
+            TwitterPrinter.getStream(jcp, System.out);
+        } else {
+            try {
+                TwitterPrinter.printAllTweets(jcp, System.out);
+            } catch (TwitterException te) {
+                te.printStackTrace();
+            }
+        }
+    }
+}
---
+++ egiby/src/main/java/ru/fizteh/fivt/students/egiby/moduletests/library/TwitterPrinter.java
@@ -0,0 +1,89 @@
+package ru.fizteh.fivt.students.egiby.moduletests.library;
+
+import com.beust.jcommander.JCommander;
+import twitter4j.*;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by egiby on 15.12.15.
+ */
+public class TwitterPrinter {
+    private static final int DEFAULT_NUMBER_OF_TWEETS = 100;
+
+    public static void printHelp(JCommander jcm) {
+        jcm.usage();
+    }
+
+    public static void printAllTweets(JCommanderParams jcp, PrintStream out) throws TwitterException {
+        Twitter twitter = new TwitterFactory().getInstance();
+        Query query = new Query(jcp.getKeyword());
+
+        if (jcp.getLocation() != null) {
+            LocationUtils.Location location = LocationUtils.getLocationByName(jcp.getLocation());
+            query.setGeoCode(location.getCoordinates(), location.getRadius(), Query.KILOMETERS);
+        }
+
+        int numberOfTweets = 0;
+
+        int limit = DEFAULT_NUMBER_OF_TWEETS;
+
+        if (jcp.getNumberTweets() != null) {
+            limit = jcp.getNumberTweets();
+        }
+
+        QueryResult result;
+        while (numberOfTweets < limit && query != null) {
+            result = twitter.search(query);
+
+            List<Status> tweets = result.getTweets();
+            for (Status tweet : tweets) {
+                if (tweet.isRetweet() && jcp.isHideRetweets()) {
+                    continue;
+                }
+
+                out.println(FormatUtils.formatTweet(tweet, false));
+
+                numberOfTweets++;
+                if (numberOfTweets == limit) {
+                    return;
+                }
+            }
+
+            query = result.nextQuery();
+        }
+    }
+
+    public static void getStream(JCommanderParams jcp, PrintStream out) {
+        StatusAdapter listener = new StatusAdapter() {
+            @Override
+            public void onStatus(Status tweet) {
+                if (jcp.isHideRetweets() && tweet.isRetweet()) {
+                    return;
+                }
+
+                out.println(FormatUtils.formatTweet(tweet, true));
+
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        twitter4j.TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(listener);
+
+        FilterQuery filter = new FilterQuery(jcp.getKeyword());
+
+        if (jcp.getLocation() != null) {
+            filter.locations(LocationUtils.getLocationByName(jcp.getLocation()).getCoordinateBox());
+        }
+
+        twitterStream.filter(filter);
+    }
+}
