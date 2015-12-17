a3905dd1beb2512942ef4ae8fc61fe7ca7234b18
egiby <egiby@egiby>
2015-12-17 20:01:42 +0300

    Init threads

---
+++ egiby/src/main/java/ru/fizteh/fivt/students/egiby/threads/BlockingQueue.java
@@ -0,0 +1,7 @@
+package ru.fizteh.fivt.students.egiby.threads;
+
+/**
+ * Created by egiby on 17.12.15.
+ */
+public class BlockingQueue {
+}
---
+++ egiby/src/main/java/ru/fizteh/fivt/students/egiby/threads/Call.java
@@ -0,0 +1,10 @@
+package ru.fizteh.fivt.students.egiby.threads;
+
+/**
+ * Created by egiby on 17.12.15.
+ */
+public class Call {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
