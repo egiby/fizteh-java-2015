ebf4d0f45a413b0ce44b219468612fce38a562b2
egiby <egiby@egiby>
2015-12-18 03:54:29 +0300

    There were some troubles in tests

--- egiby/src/test/java/ru/fizteh/fivt/students/egiby/threads/BlockingQueueTest.java
+++ egiby/src/test/java/ru/fizteh/fivt/students/egiby/threads/BlockingQueueTest.java
@@ -13,37 +13,26 @@ import java.util.List;
 
 public class BlockingQueueTest {
     private static final Object MONITOR = new Object();
-    private static final Object WAIT_FIRST = new Object();
-    private static volatile int current = 1;
 
     private static class ThreadTest extends Thread {
-        protected static BlockingQueue<Integer> queue = new BlockingQueue<>(15);
+        protected static BlockingQueue<Integer> queue = new BlockingQueue<>(150);
     }
 
     private static class ThreadPush extends ThreadTest {
         private List<Integer> list;
-        private int id;
 
-        ThreadPush(List<Integer> to, int n) {
+        ThreadPush(List<Integer> to) {
             list = to;
-            id = n;
         }
 
         @Override
         public void run() {
-            synchronized (WAIT_FIRST) {
-                while (id != current) {
-                    try {
-                        WAIT_FIRST.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                queue.offer(list);
-                current++;
-                WAIT_FIRST.notifyAll();
+            try {
+                sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+            queue.offer(list);
         }
     }
 
@@ -79,14 +68,14 @@ public class BlockingQueueTest {
             to2.add(i * 2 + 1);
         }
 
-        ThreadPush threadPush1 = new ThreadPush(to1, 1);
-        ThreadPush threadPush2 = new ThreadPush(to2, 2);
+        ThreadPush threadPush1 = new ThreadPush(to1);
+        ThreadPush threadPush2 = new ThreadPush(to2);
 
         ThreadPop threadPop= new ThreadPop(to1.size() + to2.size());
 
         synchronized (MONITOR) {
-            threadPush2.start();
             threadPush1.start();
+            threadPush2.start();
             threadPop.start();
             while (answer.isEmpty()) {
                 MONITOR.wait();
