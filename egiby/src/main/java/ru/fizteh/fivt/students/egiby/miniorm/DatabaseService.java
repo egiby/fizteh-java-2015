01c88549110ec70d507ca3528f2bf3dc8f6bc6a5
egiby <egiby@egiby>
2015-12-19 04:21:40 +0300

    style fix

--- egiby/src/main/java/ru/fizteh/fivt/students/egiby/miniorm/DatabaseService.java
+++ egiby/src/main/java/ru/fizteh/fivt/students/egiby/miniorm/DatabaseService.java
@@ -8,7 +8,6 @@ import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
